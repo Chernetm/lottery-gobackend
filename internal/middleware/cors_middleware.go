36223beb66ca3269d0969f60b71ca0f1d9b85ad8
Chernetm <136773904+Chernetm@users.noreply.github.com>
2026-03-08 06:10:47 +0300

    cors link is changed

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// https://ethiodigitallottery.vercel.app/
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://habeshalottery.vercel.app")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://ethiodigitallottery.vercel.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers",
 			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
